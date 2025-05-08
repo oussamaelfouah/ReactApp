72c7fe0fa202a99175cfda15552420ffaccb1ce1
oussamaelfouah <oelfouah@gmail.com>
2025-05-08 13:59:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import Button from "./components/Button.js";
+import { GoBriefcase, GoGlobe } from "react-icons/go";
+
+function App() {
+  const handleClick = () => {
+    console.log("it clicked !!");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button className="mb-5 " onClick={handleClick} outline primary>
+          <GoBriefcase />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now!
+          <GoGlobe />
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded outline>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
